9e1707bafeaf43f8ae19da4cd429e63d562e3ce6
simonewalker <simonewalker@deloitte.co.uk>
2023-01-08 18:31:24 +0000

    Adding input onChange event inline

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, ChangeEvent, useState} from 'react';
 
 //You give types to props in a component through the interface.
 //An interface is a way to describe an obj.
@@ -9,12 +9,16 @@ interface Props {
    // getName: (name: string) => string;   // function that returns a string and passes an argument that is a string
 }
 
-export const Person: FC<Props> =({name, age, email}) => {  //destructure prop arguments
+export const Person: FC<Props> =({name, age, email}) => {
+    const [country, setCountry] = useState<string | null>(null)
+//destructure prop arguments
     return (
         <div>
             <h1>{name}</h1>
             <h1>{email}</h1>
             <h1>{age}</h1>
+            <input placeholder="Write down your country..." onChange={(event) => {setCountry(event.target.value)}}/>
+            {country}
         </div>
     )
 
