a43602be1dea7e0891de440e61f1b767d40fdd6a
simonewalker <simonewalker@deloitte.co.uk>
2023-01-09 08:57:38 +0000

    Added context api to app and created empty Interface and Enums files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React, {FC} from 'react';  //of type Functional Component
+import React, {FC, createContext} from 'react'; //of type Functional Component
 import './App.css';
-import {Person} from "./components/Person";
+import { Person, HairColor} from "./components/Person";
+
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
+
+const AppContext = createContext<AppContextInterface | null >(null)
 
 const App: FC = () =>  {
+  const contextValue: AppContextInterface = {
+    name: "Pedro",
+    age: 20,
+    country: "Brazil"
+  }
 
   return (
-    <div className="App">
-      <Person name="pedro" age={20} email="pedro@email.com" />
-    </div>
+    <AppContext.Provider value={contextValue}>
+        <div className="App">
+            <Person name="pedro" age={20} email="pedro@email.com" hairColor={HairColor.Blonde} />
+        </div>
+    </AppContext.Provider>
+
   );
 }
 
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,30 +1,47 @@
-import {FC, ChangeEvent, useState} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
+
+// Enums defines options. So it can be an option inside the props
+export enum HairColor {
+    Blonde= "Your hair is blonde good for you",
+    Brown="Your hair is brown. Cool hair color",
+    Pink="Your hair is pink. Wow that is so cool",
+}
 
 //You give types to props in a component through the interface.
-//An interface is a way to describe an obj.
+//An interface is a way to describe an obj. You define an obj
 interface Props {
     name: string;
     age: number;
     email: string;
+    hairColor: HairColor;
    // getName: (name: string) => string;   // function that returns a string and passes an argument that is a string
 }
 
-export const Person: FC<Props> = ({name, age, email}) => {
+
+//destructure prop arguments
+export const Person: FC<Props> = ({name, age, email, hairColor}) => {
     const [country, setCountry] = useState<string | null>(null)
+
+    type NameType = "Pedro" | "Jack"
+    const nameForType: NameType = "Jack"
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setCountry(event.target.value)
     }
 
-//destructure prop arguments
+
+
     return (
         <div>
+            <>
             <h1>{name}</h1>
             <h1>{email}</h1>
             <h1>{age}</h1>
             <input placeholder="Write down your country..." onChange={handleChange}/>
             {country}
+            {hairColor}
+            </>
         </div>
-    )
+)
 
 }
